dc37e54cfcdb82e3fd95e5713a3c58a110d6793d
Gary Deschaines <gedeschaines@gmail.com>
2018-07-22 10:16:05 -0500

    Added MATLAB like tic/toc and warning functions.

--- robot/utility.py
+++ robot/utility.py
@@ -4,93 +4,134 @@ Python toolbox utility and helper functions.
 
 Python implementation by: Luis Fernando Lara Tobar and Peter Corke.
 Based on original Robotics Toolbox for Matlab code by Peter Corke.
-Permission to use and copy is granted provided that acknowledgement of
-the authors is made.
+Permission to use and copy is granted provided that acknowledgement
+of the authors is made.
 
 @author: Luis Fernando Lara Tobar and Peter Corke
 """
 
 from numpy import *
+import time
 
 
+################ Matlab tic/toc timer
+
+global t_bgn
+t_bgn = time.time()
+
+def tic():
+    global t_bgn
+    t_beg = time.time()
+    return
+
+def toc():
+    global t_beg
+    t_end = time.time()
+    return 'Elapsed time is %.5f seconds.' % (t_end - t_bgn)
+
+################ data shape queries
+
+def isempty(v):
+    if v is None:
+        return True
+    try:
+        return len(v) == 0
+    except TypeError:
+        return False
+        
 def ishomog(tr):
     """
     True if C{tr} is a 4x4 homogeneous transform.
-    
-    @note: Only the dimensions are tested, not whether the rotation submatrix
-    is orthonormal.
-    
+
+    @note: Only the dimensions are tested, not whether the rotation
+    submatrix is orthonormal.
+
     @rtype: boolean
     """
-    
-    return tr.shape == (4,4)
-
-
+    if tr is None:
+        return False
+    try:
+        size = tr.shape
+        return size == (4,4)
+    except AttributeError:
+        return False
 
 def isrot(r):
     """
-    True if C{tr} is a 3x3 matrix.
-    
+    True if C{r} is a 3x3 matrix.
+
     @note: Only the dimensions are tested, not whether the matrix
     is orthonormal.
-    
+
     @rtype: boolean
     """
-    return r.shape == (3,3)
-
+    if r is None:
+        return False
+    try:
+        size = r.shape
+        return size == (3,3)
+    except AttributeError:
+        return False
 
 def isvec(v, l=3):
     """
-    True if C{tr} is an l-vector.  
-    
+    True if C{v} is an l-vector.
+
     @param v: object to test
     @type l: integer
     @param l: length of vector (default 3)
-   
+
     @rtype: boolean
     """
-    return v.shape == (l,1) or v.shape == (1,l) or v.shape == (l,)
-
+    if v is None:
+        return False
+    try:
+        size = v.shape
+        return size == (l,1) or size == (1,l) or size == (l,)
+    except AttributeError:
+        return False
 
 def numcols(m):
     """
     Number of columns in a matrix.
-    
+
     @type m: matrix
     @return: the number of columns in the matrix.
     return m.shape[1];
     """
     return m.shape[1];
-    
+
 def numrows(m):
     """
     Number of rows in a matrix.
-    
+
     @type m: matrix
     @return: the number of rows in the matrix.
     return m.shape[1];
     """
     return m.shape[0];
 
+
 ################ vector operations
 
 def unit(v):
     """
     Unit vector.
-    
+
     @type v: vector
     @rtype: vector
     @return: unit-vector parallel to C{v}
     """
     return mat(v / linalg.norm(v))
-    
+
+
 def crossp(v1, v2):
     """
     Vector cross product.
-    
+
     @note Differs from L{numpy.cross} in that vectors can be row or
     column.
-    
+
     @type v1: 3-vector
     @type v2: 3-vector
     @rtype: 3-vector
@@ -106,17 +147,18 @@ def crossp(v1, v2):
     v[2] = v1[0]*v2[1] - v1[1]*v2[0]
     return v
 
+
 ################ misc support functions
 
 def arg2array(arg):
     """
-    Convert a 1-dimensional argument that is either a list, array or matrix to an 
+    Convert a 1-dimensional argument that is either a list, array or matrix to an
     array.
-    
+
     Useful for functions where the argument might be in any of these formats:::
             func(a)
             func(1,2,3)
-            
+
             def func(*args):
                 if len(args) == 1:
                     v = arg2array(arg[0]);
@@ -125,7 +167,7 @@ def arg2array(arg):
              .
              .
              .
-    
+
     @rtype: array
     @return: Array equivalent to C{arg}.
     """
@@ -141,31 +183,37 @@ def arg2array(arg):
 
     elif isinstance(arg, (int, float, float32, float64)):
         return array([arg]);
-        
-    raise ValueError;
-        
 
+    raise ValueError;
+
+################ error and warning handlers 
 
 import traceback;
 
 def error(s):
     """
-    Common error handler.  Display the error string, execute a traceback then raise
+    Common error handler. Display the error string, execute a traceback then raise
     an execption to return to the interactive prompt.
     """
-    print 'Robotics toolbox error:', s
+    print("Robotics toolbox error: %s" % s)
 
     #traceback.print_exc();
-    raise ValueError
+    raise ValueError()
+    
+    
+def warning(sfnc,smsg):
+    """
+    Common warning handler. Display the warning strings - function and message.
+    """
+    print("%s: %s" % (sfnc, smsg))
     
     
 if __name__ == "__main__":
-    print arg2array(1)
-    print arg2array(1.0); 
-    print arg2array( mat([1,2,3,4]) )
-    print arg2array( mat([1,2,3,4]).T )
-    print arg2array( array([1,2,3]) );
-    print arg2array( array([1,2,3]).T );
-    print arg2array( [1,2,3]);
+    print("%s" % arg2array(1))
+    print("%s" % arg2array(1.0))
+    print("%s" % arg2array( mat([1,2,3,4]) ))
+    print("%s" % arg2array( mat([1,2,3,4]).T ))
+    print("%s" % arg2array( array([1,2,3]) ))
+    print("%s" % arg2array( array([1,2,3]).T ))
+    print("%s" % arg2array( [1,2,3]))
 
-    
