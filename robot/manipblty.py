ff15622f2c73f0811a3696459bd30cb2c37815da
Gary Deschaines <gedeschaines@gmail.com>
2018-07-22 10:34:49 -0500

    Renamed from manipulabilty.py to match references in other documentation.

--- robot/manipblty.py
+++ robot/manipblty.py
@@ -3,13 +3,11 @@ Robot manipulability operations.
 
 Python implementation by: Luis Fernando Lara Tobar and Peter Corke.
 Based on original Robotics Toolbox for Matlab code by Peter Corke.
-Permission to use and copy is granted provided that acknowledgement of
-the authors is made.
+Permission to use and copy is granted provided that acknowledgement
+of the authors is made.
 
 @author: Luis Fernando Lara Tobar and Peter Corke
 """
-
-
 
 from numpy import *
 from robot.jacobian import jacob0
@@ -17,24 +15,25 @@ from robot.dynamics import inertia
 from numpy.linalg import inv,eig,det,svd
 from robot.utility import *
 
-def manipulability(robot, q, which = 'yoshikawa'):
-    '''
-    MANIPLTY Manipulability measure
 
-        - M = MANIPLTY(ROBOT, Q)
-        - M = MANIPLTY(ROBOT, Q, WHICH)
+def manipblty(robot, q, *args):
+    '''
+    MANIPBLTY Manipulability measure
+
+        - M = MANIPBLTY(ROBOT, Q)
+        - M = MANIPBLTY(ROBOT, Q, WHICH)
 
     Computes the manipulability index for the manipulator at the given pose.
 
     For an n-axis manipulator Q may be an n-element vector, or an m x n
     joint space trajectory.
 
-    If Q is a vector MANIPLTY returns a scalar manipulability index.
-    If Q is a matrix MANIPLTY returns a column vector of  manipulability 
+    If Q is a vector MANIPBLTY returns a scalar manipulability index.
+    If Q is a matrix MANIPBLTY returns a column vector of manipulability 
     indices for each pose specified by Q.
 
-    The argument WHICH can be either 'yoshikawa' (default) or 'asada' and
-    selects one of two manipulability measures.
+    The argument WHICH can be either 'y[oshikawa]' (default) or 'a[sada]'
+    and selects one of two manipulability measures.
     Yoshikawa's manipulability measure gives an indication of how far 
     the manipulator is from singularities and thus able to move and 
     exert forces uniformly in all directions.
@@ -53,27 +52,32 @@ def manipulability(robot, q, which = 'yoshikawa'):
     n = robot.n
     q = mat(q)
     w = array([])
-    if 'yoshikawa'.startswith(which):
-            if numrows(q)==1:
-                    return yoshi(robot,q)
-            for Q in q:
-                    w = concatenate((w,array([yoshi(robot,Q)])))
-    elif 'asada'.startswith(which):
-            if numrows(q)==1:
-                    return asada(robot,q)
-            for Q in q:
-                    w = concatenate((w,array([asada(robot,Q)])))
+    
+    which = 'yoshikawa'
+    if args is not None and (len(args) > 0):
+         which = '%s' % args[0]
+    
+    if which.startswith('y'):
+        if numrows(q)==1:
+            return yoshi(robot,q)
+        for Q in q:
+            w = concatenate((w,array([yoshi(robot,Q)])))
+    elif which.startswith('a'):
+        if numrows(q)==1:
+            return asada(robot,q)
+        for Q in q:
+            w = concatenate((w,array([asada(robot,Q)])))
     return mat(w)
 
 def yoshi(robot,q):
-        J = jacob0(robot,q)
-        return sqrt(det(J*J.T))
+    J = jacob0(robot,q)
+    return sqrt(det(J*J.T))
 
 def asada(robot,q):
-        J = jacob0(robot,q)
-        Ji = inv(J)
-        M = inertia(robot,q)
-        Mx = Ji.T*M*Ji
-        e = eig(Mx)[0]
-
-        return real( e.min(0)/e.max(0) )
+    J = jacob0(robot,q)
+    Ji = inv(J)
+    M = inertia(robot,q)
+    Mx = Ji.T*M*Ji
+    e = eig(Mx)[0]
+    return real( e.min(0)/e.max(0) )
+        
