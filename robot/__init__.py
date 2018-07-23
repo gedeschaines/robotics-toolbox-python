7c8b29a52f22020b1966dd0126e32fa138512a62
Gary Deschaines <gedeschaines@gmail.com>
2018-07-23 01:18:19 -0500

    Updated information on RTB for Python.

--- robot/__init__.py
+++ robot/__init__.py
@@ -25,11 +25,12 @@ Peter Corke 2007
 """
 
 __info__ = """
-Robotics Toolbox for Python
+Robotics Toolbox for Python - Update 1
 Based on Matlab Toolbox Version 7  April-2002
+Updated kinematics and dynamics based on Matlab Toolbox Version 9  July-2018
 
 What's new.
-  Readme      - New features and enhancements in this version.
+  Readme.md   - New features and enhancements in this version.
 
 Homogeneous transformations
   eul2tr      - Euler angle to transform
@@ -38,6 +39,8 @@ Homogeneous transformations
   roty        - transform for rotation about Y-axis
   rotz        - transform for rotation about Z-axis
   rpy2tr      - roll/pitch/yaw angles to transform
+  rt2tr       - rotation matrix and translation vector to transform
+  tr2angvec   - convert rotation matrix to angle-vector form
   tr2eul      - transform to Euler angles
   tr2rot      - transform to rotation submatrix
   tr2rpy      - transform to roll/pitch/yaw angles
@@ -67,22 +70,27 @@ Dynamics
   accel       - compute forward dynamics
   cinertia    - compute Cartesian manipulator inertia matrix
   coriolis    - compute centripetal/coriolis torque
+  fdyn        - forward dynamic integration using 'lsode' method
   friction    - joint friction
-  ftrans      - transform force/moment
   gravload    - compute gravity loading
   inertia     - compute manipulator inertia matrix
   itorque     - compute inertia torque
   nofriction  - remove friction from a robot object
-  rne         - inverse dynamics
+  rne         - inverse dynamics using Recursive Newton-Euler
+  wtrans      - transform wrench (force/moment 6-vector)
 
 Trajectory generation
   ctraj       - Cartesian trajectory
   jtraj       - joint space trajectory
+  mpoly       - multi-axis trajactory
+  tpoly       - scalar polynomial trajectory
   trinterp    - interpolate transform s
 
 Graphics
-  drivebot    - drive a graphical  robot
-  plot        - plot/animate robot
+  qplot       - joint angle trajectory plot
+  rbplot      - plot/animate robot
+  trplot      - plot transform frame plot
+  tranimate   - animate transform frame
 
 Other
   manipblty   - compute manipulability
@@ -90,11 +98,14 @@ Other
 
 Creation of robot models.
   link        - construct a robot link object
+  phantomx    - PhantomX Pincher Robot data
   puma560     - Puma 560 data
   puma560akb  - Puma 560 data (modified Denavit-Hartenberg)
   robot       - construct a robot object
   stanford    - Stanford arm data
   twolink     - simple 2-link example
+  fourlink2d  - simple 4-link 2D planar manipulator arm
+  fourlink3d  - simple 4-link 3D upright manipultor arm
 """
 
 ######################
