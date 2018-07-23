45645bfbfdf7c59c823f16871d430f8b539afeaa
Gary Deschaines <gedeschaines@gmail.com>
2018-07-23 01:17:58 -0500

    Updated information on RTB for Python.

--- robot/ptb.py
+++ robot/ptb.py
@@ -1,10 +1,12 @@
 class ptb:
     '''
-# Robotics Toolbox.
-# Version 7  April-2002
+# Robotics Toolbox for Python - Update 1
+#
+# Based on Matlab Toolbox Version 7  April-2002
+# Updated kinematics and dynamics based on Matlab Toolbox Version 9  July-2018
 #
 # What's new.
-#   Readme      - New features and enhancements in this version.
+#   Readme.md   - New features and enhancements in this version.
 #
 # Homogeneous transformations
 #   eul2tr      - Euler angle to transform 
@@ -12,7 +14,9 @@ class ptb:
 #   rotx        - transform for rotation about X-axis 
 #   roty        - transform for rotation about Y-axis 
 #   rotz        - transform for rotation about Z-axis 
-#   rpy2tr      - roll/pitch/yaw angles to transform 
+#   rpy2tr      - roll/pitch/yaw angles to transform
+#   rt2tr       - rotation matrix and translation vector to transform
+#   tr2angvec   - convert rotation matrix to angle-vector form
 #   tr2eul      - transform to Euler angles 
 #   tr2rot      - transform to rotation submatrix
 #   tr2rpy      - transform to roll/pitch/yaw angles
@@ -41,38 +45,53 @@ class ptb:
 # Dynamics
 #   accel       - compute forward dynamics
 #   cinertia    - compute Cartesian manipulator inertia matrix 
-#   coriolis    - compute centripetal/coriolis torque 
+#   coriolis    - compute centripetal/coriolis torque
+#   fdyn        - forward dynamic integration using 'lsode' method
 #   friction    - joint friction
-#   ftrans      - transform force/moment 
 #   gravload    - compute gravity loading 
 #   inertia     - compute manipulator inertia matrix 
 #   itorque     - compute inertia torque 
 #   nofriction  - remove friction from a robot object 
-#   rne         - inverse dynamics 
+#   rne         - inverse dynamics using Recursive Newton-Euler
+#   wtrans      - transform wrench (force/moment 6-vector)
 #   
 # Trajectory generation
 #   ctraj       - Cartesian trajectory 
-#   jtraj       - joint space trajectory 
+#   jtraj       - joint space trajectory
+#   mpoly       - multi-axis trajactory
+#   tpoly       - scalar polynomial trajectory
 #   trinterp    - interpolate transform s
 #   
 # Graphics
-#   drivebot    - drive a graphical robot 
-#   plot        - plot/animate robot 
-#   
+#   qplot       - joint angle trajectory plot
+#   rbplot      - plot/animate robot
+#   trplot      - plot transform frame plot
+#   tranimate   - animate transform frame
+#
 # Other
 #   manipblty   - compute manipulability 
 #   unit        - unitize a vector
 #
 # Creation of robot models.
-#   link        - construct a robot link object 
+#   link        - construct a robot link object
+#   phantomx    - PhantomX Pincher Robot data
 #   puma560     - Puma 560 data 
 #   puma560akb  - Puma 560 data (modified Denavit-Hartenberg)
 #   robot       - construct a robot object 
 #   stanford    - Stanford arm data 
-#   twolink     - simple 2-link example 
+#   twolink     - simple 2-link example
+#   fourlink2d  - simple 4-link 2D planar manipulator arm
+#   fourlink3d  - simple 4-link 3D upright manipultor arm
 #
 # Demonstrations.
-#   rtdemo      - toolbox demonstration
+#   transform.py    transforms and quaternions
+#   trajectory.py   trajectories
+#   animation.py    animations
+#   fkine.py        forward kinematics
+#   jacobian.py     Jacbians
+#   ikine.py        inverse dynamics
+#   idyn.py         inverse dynamics, RNE etc.
+#   fdyn.py         forward dynamics
 #   
 
 # Copyright (C) 2002, by Peter I. Corke
