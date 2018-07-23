13c3dc7edc09cfd0f699aa48cfdb28cd893f0af9
Gary Deschaines <gedeschaines@gmail.com>
2018-07-23 14:45:10 -0500

    Trajectory details tutorial derived from RBT for MATLAB demos/traj.m script.

--- demo/traj.py
+++ demo/traj.py
@@ -12,8 +12,8 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 # 
-# You should have received a copy of the GNU Leser General Public License
-# along with RTB.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public
+# License along with RTB.  If not, see <http://www.gnu.org/licenses/>.
 #
 # http://www.petercorke.com
 
