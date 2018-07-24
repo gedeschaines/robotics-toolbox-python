0f0634c77b78b4c820eb542cfd4764c2caef9570
Gary Deschaines <gedeschaines@gmail.com>
2018-07-24 14:34:52 -0500

    Initial version. New comparitive test against RTB for Python kine_p560m.py.

---
+++ Octave/rvctools/robot/tests/kine_p560m.m
@@ -0,0 +1,20 @@
+% Test inverse kinematics for Puma 560 standard
+
+echo off
+
+mdl_puma560akb;
+p560m
+
+echo on
+
+% **** Kinematics Test ****
+
+T0 = fkine(p560m, [0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+q0 = ikine(p560m, T0, 'alpha', 0.4, 'ilimit', 2000)
+fkine(p560m, q0)
+
+T1 = fkine(p560m, [pi/2, -pi/4, 3*pi/4, -pi/8, 0.0, -pi/5])
+q1 = ikine(p560m, T1, 'alpha', 0.4, 'ilimit', 2000)
+fkine(p560m, q1)
+
+echo off
